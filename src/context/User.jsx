5f8cf9b70199eaf8fdd4cb821ecd8cdf2a714dce
k00w <112608822+devkoow@users.noreply.github.com>
2024-03-01 13:12:42 +0900

    [feat] sendTime을 LocalDateTime에 상응하는 toISOString으로 저장

--- src/context/User.jsx
+++ src/context/User.jsx
@@ -65,7 +65,7 @@ export const UserProvider = ({ children }) => {
       } else {
         setEmailExists(false);
         setRandomNum(response.data.randomNum);
-        setSendTime(new Date.getTime()); // 발급 시간 저장
+        setSendTime(new Date.toISOString()); // 발급 시간 저장
         setExpireTime(response.data.expireTime); // 만료 시간 저장
         alert('인증번호가 발송되었습니다');
         console.log(`발급된 인증번호 : ${randomNum}`);
@@ -91,7 +91,7 @@ export const UserProvider = ({ children }) => {
       if (response.data.exists) {
         setEmailExists(true);
         setRandomNum(response.data.randomNum);
-        setSendTime(new Date.getTime()); // 발급 시간 저장
+        setSendTime(new Date.toISOString()); // 발급 시간 저장
         setExpireTime(response.data.expireTime); // 만료 시간 저장
         alert('인증번호가 발송되었습니다');
         console.log(`발급된 인증번호 : ${randomNum}`);
