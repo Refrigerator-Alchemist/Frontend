b402013a6f3c5febf459f478c3a53738f490d381
k00w <112608822+devkoow@users.noreply.github.com>
2024-03-10 22:54:03 +0900

    [feat] fetchLoginData를 .then .catch를 사용하여 프로미스 처리

--- src/pages/LoginSuccess.jsx
+++ src/pages/LoginSuccess.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useUserState } from '../context/UserContext.jsx';
+import { useUserState, useUserDispatch } from '../context/UserContext.jsx';
 import { useNavigate } from 'react-router-dom';
 
 export default function LoginSuccess() {
@@ -7,19 +7,73 @@ export default function LoginSuccess() {
   const [email, setEmail] = useState('');
   const [socialType, setSocialType] = useState('');
   const [socialId, setSocialId] = useState('');
+  const user = useUserState();
 
-  const { fetchLoginData, user } = useUserState();
+  const dispatch = useUserDispatch();
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchLoginData();
+  const SET_USER = 'SET_USER';
 
-    setAccessToken(localStorage.getItem('accessToken'));
-    setEmail(localStorage.getItem('email'));
-    setSocialId(localStorage.getItem('socialId'));
-    setSocialType(localStorage.getItem('socialType'));
-  }, [fetchLoginData]);
+  useEffect(() => {
+    const fetchLoginData = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const accessToken = urlParams.get('accessToken');
+      const email = urlParams.get('email');
+      const socialId = urlParams.get('socialId');
+      const socialType = urlParams.get('socialType');
+
+      console.log(`액세스 토큰 : ${accessToken}`);
+      console.log(`이메일 : ${email}`);
+      console.log(`소셜 ID : ${socialId}`);
+      console.log(`소셜 타입 : ${socialType}`);
+      console.log('⭕ 데이터 받아오는 건 문제없음');
+
+      // ▶ 4개 데이터 받아왔는지 판단
+      if (accessToken && email && socialType && socialId) {
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('email', email);
+        localStorage.setItem('socialId', socialId);
+        localStorage.setItem('socialType', socialType);
+
+        // ▶ 유저 데이터 저장
+        let user = {
+          accessToken: localStorage.getItem('accessToken'),
+          email: localStorage.getItem('email'),
+          uid: localStorage.getItem('socialId'),
+          socialType: localStorage.getItem('socialType'),
+        };
+
+        console.log(`유저 데이터 저장 완료 : ${user}`);
+
+        // ▶ dispatch로 리듀서에 저장
+        dispatch({ type: SET_USER, user });
+        window.alert('⭕ dispatch 문제없음');
+        console.log('⭕ dispatch 문제없음');
+
+        setAccessToken(localStorage.getItem('accessToken'));
+        setEmail(localStorage.getItem('email'));
+        setSocialId(localStorage.getItem('socialId'));
+        setSocialType(localStorage.getItem('socialType'));
+
+        return user;
+      } else {
+        // ▶ 데이터가 하나라도 모자라면 발생
+        console.log('서버에서 데이터 받는 중 문제 발생');
+        window.alert('서버에서 데이터 받는 중 문제 발생');
+      }
+    };
+
+    fetchLoginData()
+      .then((user) => {
+        if (user) {
+          window.alert('⭕ SNS 로그인 데이터 저장 완료');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [dispatch]);
 
   return (
     <section>
